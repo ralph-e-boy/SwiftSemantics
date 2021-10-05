3a9446546e9e32a6819a6cf776a8174214501beb
Ralph Seaman <ralph.seaman@mlb.com>
2021-10-05 15:41:48 -0400

    nodep

--- Package.swift
+++ Package.swift
@@ -14,9 +14,10 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "SwiftSyntax",
+        /*.package(name: "SwiftSyntax",
                  url: "https://github.com/apple/swift-syntax.git",
                  .revision("swift-5.3-RELEASE")),
+        */
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
